3960ab825947b1e857ed16b0314fa2071af858ed
Usman Malik <usmanmalik740@gmail.com>
2023-04-09 21:33:17 +0500

    Model API Final

--- main.py
+++ main.py
@@ -6,31 +6,29 @@ import joblib
 app = Flask(__name__)
 
 
+# @app.route('/', methods=['GET'])
+# def index():
+#     return 'Welcome to my Flask app!'
+
+
 @app.route('/', methods=['GET'])
-def index():
-    return 'Welcome to my Flask app!'
-
-
-@app.route('/search', methods=['GET'])
 def search():
 
 # Load the GTRS_Final_Model file
-    # model = joblib.load('GTRS_Final_Model.joblib2')
+    model = joblib.load('GTRS_Final_Model.joblib2')
 
-    # # Create a sample DataFrame for prediction
-    # df = pd.DataFrame({'feat1': [1, 2, 3], 'feat2': [4, 5, 6], 'feat3': [7, 8, 9]})
+    # Create a sample DataFrame for prediction
+    df = pd.DataFrame({'feat1': [1, 2, 3], 'feat2': [4, 5, 6], 'feat3': [7, 8, 9]})
     
-    # # Make predictions using the model
-    # predictions = model.predict(df)
+    # Make predictions using the model
+    predictions = model.predict(df)
 
-    # print(predictions)
+    print(predictions)
     
-    # query = request.args.get('q')
-    # results = predictions # your search logic here, e.g. querying a database or calling an API
+    query = request.args.get('q')
+    results = predictions # your search logic here, e.g. querying a database or calling an API
 
-    # return jsonify({'results': results})
-    
-    return 'Welcome to Model Screen!'
+    return jsonify({'results': results})
 
 if __name__ == '__main__':
     app.run(debug=True)
