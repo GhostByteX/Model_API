96682e0f318d326234d90e792586efbbb70d226b
Usman Malik <usmanmalik740@gmail.com>
2023-04-10 21:22:03 +0500

    Policy Updated

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify
 from GTRS import TWC_GTRS_MODEL
+from flask_cors import CORS
 import pandas as pd
 import joblib
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def search():
