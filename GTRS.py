f846352b01b47ebee814a22f477c811e2efbb409
Usman Malik <usmanmalik740@gmail.com>
2023-04-10 23:58:42 +0500

    Usman - Updated

--- GTRS.py
+++ GTRS.py
@@ -1,13 +1,5 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import mean_squared_error
-from matplotlib import pyplot as plt
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import OrdinalEncoder
 import joblib
 import pickle
 
